48842d6c5669b24458a233479133265cd5669480
oleols1 <144025972+oleols1@users.noreply.github.com>
2024-09-27 13:52:15 +0200

    Opprettet MapController.cs og MapModel.cs

---
+++ Gruppe_oppgave1/Controllers/MapController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Gruppe_oppgave1.Controllers
+{
+    public class MapController : Controller
+    {
+        [HttpGet]
+        public IActionResult Map()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Map(MapModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                positions.Add(model);
+                return View("MapModel", positions);
+            }
+            return View(model);
+        }
+    }
+
+}
---
+++ Gruppe_oppgave1/Models/MapModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Gruppe_oppgave1.Models
+{
+    public class MapModel
+    {
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public string Description { get; set; }
+    }
+}
