6756152a64fef290aa0c88067a6caa7c066c93f2
Sigurd Mæland <sigmal2003@hotmail.com>
2024-09-27 13:00:32 +0200

    Initial commit

---
+++ Gruppe_oppgave1/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Gruppe_oppgave1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
