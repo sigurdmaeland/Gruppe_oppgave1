fd155c9e0f468d6c76f52d2073b172b656026224
Sigurd Mæland <sigmal2003@hotmail.com>
2024-09-29 20:10:28 +0200

    endringer

--- Gruppe_oppgave1/Controllers/HomeController.cs
+++ Gruppe_oppgave1/Controllers/HomeController.cs
@@ -7,6 +7,7 @@ namespace Gruppe_oppgave1.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private static List<MapModel> positions = new List<MapModel>();
 
     public HomeController(ILogger<HomeController> logger)
     {
@@ -22,6 +23,26 @@ public class HomeController : Controller
     {
         return View();
     }
+   
+    [HttpGet]
+    public IActionResult MapModel()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult MapModel(MapModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            positions.Add(model);
+            return View("MapFeedback", positions);
+        }
+        return View(model);
+    }
+    
+    
+
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
